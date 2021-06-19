f94459e66c1eb458dd69949a816a010edf5b6fc6
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-06-19 10:36:18 -0300

    Ajustando responsividade - homepage

--- js/script.js
+++ js/script.js
@@ -7,22 +7,12 @@ function buscarElemento(element) {
     searchBar.value = element.innerText
 }
 
-function animate(element) {
-    element.classList.remove('animate')
-    window.setTimeout(() => {
-        element.classList.add('animate')
-    }, 50)
-}
-
 const slide = setInterval(() => {
     if (sliderInputs[0].checked) {
         sliderInputs[1].checked = true
-        animate(sliderBanners[1])
     } else if (sliderInputs[1].checked) {
         sliderInputs[2].checked = true
-       animate(sliderBanners[2])
     } else {
         sliderInputs[0].checked = true
-       animate(sliderBanners[0])
     }
 }, 7000)
