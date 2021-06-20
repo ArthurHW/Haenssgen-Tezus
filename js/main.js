e821e0fc8316086f80c2954b717bde33c3943d33
Arthur Henrique Wiebusch <arturh07@gmail.com>
2021-06-20 10:31:05 -0300

    Terminando responsividade do footer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const searchBar = document.querySelector('#pesquisa')
 const openMobileMenuButton = document.querySelector('.open-menu-mobile')
 const menuMobileContent = document.querySelector('.menu-mobile-content')
 const closeMobileMenuButton = document.querySelector('header .menu-mobile .menu-mobile-content .close-container .close')
-const openSubmenuArrows = document.querySelectorAll('header .menu-mobile .menu-mobile-content ul li .seta')
+const openSubmenuArrows = document.querySelectorAll('.seta')
 const lupa = document.querySelector('header .container .lupa-carrinho-mobile .lupa')
 const searchBarContainer = document.querySelector('header .busca ')
 
@@ -27,6 +27,3 @@ openSubmenuArrows.forEach(seta => {
 lupa.addEventListener('click', () => {
     searchBarContainer.classList.toggle('active')
 })
-
-
-
